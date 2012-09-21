cc886b028ae5bd10170bda182e7b2f87099fb31b
Anita Patel <anita@anitapatel.com>
2012-09-21 09:51:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScavengerHunt::Application.config.secret_token = '6109dcbbc5e5da44bd2c4cb21e04d5e2f852a46b182e987e3a862c11706629838c74a553443f27bb8799d07a04b764410592a7bdc9a205baf12c9e8531176168'

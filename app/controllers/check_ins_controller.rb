d67179681fd5793d00bb771a27af58541a0c344f
Anita Patel <anita@anitapatel.com>
2012-10-26 14:25:32 -0400

    showing check-ins for admin by team

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,2 +1,10 @@
 class CheckInsController < ApplicationController
+  def destroy
+    @check_in = CheckIn.find(params[:id])
+    @check_in.destroy
+
+    respond_to do |format|
+      format.html { redirect_to clue_maps_url }
+    end
+  end
 end

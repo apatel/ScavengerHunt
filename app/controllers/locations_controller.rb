abc5dbdd37784fd0c7d3ce107baef5b059e7532c
Anita Patel <anita@anitapatel.com>
2012-10-27 14:06:18 -0400

    changed time zone, added show for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,10 @@ class LocationsController < ApplicationController
   
   def index
     @locations = Location.all
+  end
+  
+  def show
+    @location = Location.find(params[:id])
   end  
   
   def edit
